a2f115dc4e7c920626679ca62e9d9baec95db91b
System <none>
2017-07-20 20:57:31 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const mongodb = require("mongodb");
-const mongoClient = mongodb.MongoClient();
+
+const mongo = require("mongodb");
+const mongoClient = mongo.MongoClient();
+const mongoURL = process.env.MONGO_URL;
+var mongoDB;
 
 const port = process.env.PORT || 3000;
 
 var app = express();
 
+mongoClient.connect(mongoURL, function (err, db) {
+  console.log("Connected to mongoDB @ mLab");
+  mongoDB = db;
+});
 
 
-app.listen();
+app.listen(port);
