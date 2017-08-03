807c63e0dffcba0c38efab50b85b322f2c4cfc76
System <none>
2017-08-03 16:38:56 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
+app.get("/api/imagesearch", imgSearch(req, res))
+
 app.get("/api/imagesearch/:image", function (req, res) {
   // Take image param, save it to the db, and use google images API to return search results
   var img = req.params.image;
@@ -31,17 +33,26 @@ app.get("/api/imagesearch/:image", function (req, res) {
   var pagestr = !isNaN(+pages) ? "&start=" + (+pages * 10) : "";
   var googPref = "https://www.googleapis.com/customsearch/v1?key=";
   
-  request(googPref + pAPIkey + "&cx=" + cx + "&searchType=image" + "&q=" + img + pagestr, function (err, response, body) {
+
+});
+
+function imgSearch(req, res) {
+  var query = req.params.image || req.query.query; // Capture the query coming either as a URL param or URL query
+  var pages = req.query.offset;
+  var pagestr = !isNaN(+pages) ? "&start=" + (+pages * 10) : ""; // Using unary '+' operator to convert to a number, reads easier than ParseInt()
+  var googlURLPrefix = "https://www.googleapis.com/customsearch/v1?key=";
+  
+  request(googlURLPrefix + pAPIkey + "&cx=" + cx + "&searchType=image" + "&q=" + query + pagestr, function (err, response, body) {
     if (err) console.log('Error:', err);
     var date = new Date();
-    mongoDB.collection("images").insertOne({query: img, time: date.toString()}, function (err, r) {
+    mongoDB.collection("images").insertOne({query: query, time: date.toString()}, function (err, r) {
       if (err) console.log("Error:", err);
       console.log("Documents inserted:", r.insertedCount)
     });
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     res.json(JSON.parse(body));
   });
-});
+}
 
 app.get("/api/latest/imagesearch", function (req, res) {
   // Display the xth most recent image searches performed from the db
