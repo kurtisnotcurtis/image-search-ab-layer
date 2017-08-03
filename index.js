11e0127ceaa7053ca43c8ea6eb1799a8ceff47bb
System <none>
2017-08-03 14:37:56 +0000

    Checkpoint

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ app.get("/api/imagesearch/:image", function (req, res) {
   // Take image param, save it to the db, and use google images API to return search results
   var img = req.params.image;
   request("https://www.googleapis.com/customsearch/v1?key=" + pAPIkey + "&cx=" + cx + "&searchType=image" + "&q=" + img, function (err, response, body) {
-    if (err) console.log('error:', err);
-    db.collection("images").insertOne(JSON.parse(body), function (err, r))
+    if (err) console.log('Error:', err);
+    var date = new Date();
+    mongoDB.collection("images").insertOne({query: img, time: date.toString()}, function (err, r) {
+      if (err) console.log("Error:", err);
+      console.log("Documents inserted:", r.insertedCount)
+    });
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     res.json(JSON.parse(body));
   });
